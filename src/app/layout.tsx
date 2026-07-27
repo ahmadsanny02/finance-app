3ec49c9b32c653b82205ed1ccecaa561df6b247a
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-27 16:01:46 +0700

    feat: integrate Toaster provider into root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import QueryProvider from "@/providers/query-client";
+import { Toaster } from "@/components/ui/sonner";
 
 const outfit = Outfit({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -36,6 +37,7 @@ export default function RootLayout({
         <QueryProvider>
           <TooltipProvider>
             {children}
+            <Toaster />
           </TooltipProvider>
         </QueryProvider>
       </body>
