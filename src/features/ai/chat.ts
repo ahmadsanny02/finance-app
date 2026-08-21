0488cdfb94af986f0506678ccb371a1b77c71944
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-08-21 07:47:40 +0700

    feat: integrate GoogleGenAI client factory and add handleWizardInput

--- src/features/ai/chat.ts
+++ src/features/ai/chat.ts
@@ -3,17 +3,16 @@
 import { Conversation } from "@/app/types/ai";
 import { ENVIRONMENT } from "@/config/environment";
 import { GoogleGenAI, ThinkingLevel } from "@google/genai";
-
-const ai = new GoogleGenAI({
-    apiKey: ENVIRONMENT.googleGenAIKey,
-});
+import { createAI } from "./instance";
 
 export async function handleChat(
     conversation: Conversation[],
     isThinking: boolean,
 ) {
+    const ai = createAI();
+
     const response = await ai.models.generateContent({
-        model: "gemini-3-flash-preview",
+        model: "gemini-3.5-flash",
         contents: [...conversation],
         config: {
             thinkingConfig: {
@@ -51,8 +50,10 @@ export async function* handleChatStreaming(
     conversation: Conversation[],
     isThinking: boolean,
 ) {
+    const ai = createAI()
+
     const response = await ai.models.generateContentStream({
-        model: "gemini-3-flash-preview",
+        model: "gemini-3.5-flash",
         contents: [...conversation],
         config: {
             thinkingConfig: {
@@ -146,4 +147,17 @@ export async function* handleChatStreaming(
             }
         }
     }
+}
+
+export async function handleWizardInput(message: string) {
+    const contents = `${message}`;
+
+    const ai = createAI()
+    const response = await ai.models.generateContent({
+        model: "gemini-3.5-flash",
+        contents,
+        config: {}
+    })
+
+    return response.text
 }
