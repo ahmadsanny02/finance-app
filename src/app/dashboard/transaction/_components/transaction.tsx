73d2407693a66900fdc21f168ed544d67dbddbab
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-09-14 22:57:38 +0700

    style(transaction): format transaction page wrapper component

--- src/app/dashboard/transaction/_components/transaction.tsx
+++ src/app/dashboard/transaction/_components/transaction.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import TransactionTable from "./transaction-table";
@@ -35,6 +35,5 @@ export default function Transaction() {
                 <CreateTransactionCard refetch={refetch} />
             </div>
         </div>
-
     );
 }
